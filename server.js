361118252eae208222a9c209ac216f82b526a2b4
Edward Kelly <madzenejk@gmail.com>
2025-09-03 23:57:21 -0500

    Add missing /api/trivia/questions endpoint for contest game functionality

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use('/api/contest-registrations', require('./api/contest-registrations'));
 app.use('/api/submit-game-score', require('./api/submit-game-score'));
 app.use('/api/validate-game-link', require('./api/validate-game-link'));
 app.use('/api/trivia', require('./api/trivia-track'));
+app.use('/api/trivia', require('./api/trivia-questions'));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
